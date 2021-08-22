e14c59aa29952f77f3cc704fd76771bfaf3dca85
Adam And <andadam24@gmail.com>
2021-08-22 16:03:45 -0400

    add css reset

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,10 +1,6 @@
-var currentQuestionIndex = 0;
-var time = questions.length * 15;
-var timerId;
-
-var questionsEl = document.getElementById("questions");
+const questions = document.querySelector("questions");
 var timerEl = document.getElementById("time");
-var choicesEl = document.getElementById("choices");
+const choices = document.getElementById("choices");
 var submitBtn = document.getElementById("submit");
 var startBtn = document.getElementById("start");
 var initialsEl = document.getElementById("initials");
